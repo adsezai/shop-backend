fcdfdc4c472d86e37e154ae1723e5e59e700a858
Adis Sezairi <adis.sezairi@wilken.de>
2020-03-22 16:16:07 +0100

    changed user route

--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -6,8 +6,10 @@ const userService = require('../service/User')
 
 router.get('/user/:userId', async (req, res, next) => res.send(await userService.getUser(req.params.userId)))
 
-router.get('user/:userId/items', async (req, res, next) => {
-  userService.getItemsByUser(this.params.userId)
+router.get('/user/:userId/items', async (req, res, next) => {
+  const user = await userService.getItemsByUser(req.params.userId)
+  console.log(user)
+  res.send(user)
 })
 
 module.exports = router
